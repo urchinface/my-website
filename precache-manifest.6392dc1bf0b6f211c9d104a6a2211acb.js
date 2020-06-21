e1bac705319c805571e367c7190045c3ca6ef538
Sophia <sophiastaves@gmail.com>
2020-06-21 21:32:48 +0100

    Updates

---
+++ precache-manifest.6392dc1bf0b6f211c9d104a6a2211acb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f816ae01140369e82514efae367e1b42",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "63ae0594a05d6d759db3",
+    "url": "/my-website/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "11235602ea0c1ce0a0da",
+    "url": "/my-website/static/js/2.04e7d5bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-website/static/js/2.04e7d5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63ae0594a05d6d759db3",
+    "url": "/my-website/static/js/main.7096320b.chunk.js"
+  },
+  {
+    "revision": "79efda537c9de2329c9f",
+    "url": "/my-website/static/js/runtime-main.096781a8.js"
+  }
+]);
